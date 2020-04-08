22b6a971d7b4baa7cc0d505476c2a4af5ea3dd61
Sahazeer <40063250+sahazeer123@users.noreply.github.com>
2020-04-08 07:31:49 +0530

    Works only for integers in librarary function

---
+++ gp4.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <unordered_map>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+template<typename T>
+class Graph
+{
+	unordered_map<T,list<T>>adjList;
+	T src;
+	T dummyNode;
+public:
+	explicit Graph()
+	{
+		adjList = unordered_map<T,list<T>>();
+		src = T();
+		dummyNode = T();
+	}
+	void addEdge(T u,T v,bool bidirect = true)
+	{
+		if(src == T())
+			src = u;
+		adjList[u].push_back(v);
+		if(bidirect)
+			adjList[v].push_back(u);
+	}
+	class BFSIterator
+	{
+		unordered_map<T,list<T>>adjList;
+		unordered_map<T,bool>visited;
+		queue<T>Q;
+		T current;
+	public:
+        using iterator_category = forward_iterator_tag;
+        using value_type = int;
+        using difference_type = int;
+        using pointer = int*;
+        using reference = int&;
+ 
+		explicit BFSIterator(T src,unordered_map<T,list<T>>& list)
+		{
+			adjList = list;
+			current = src;
+			if(src != T())
+			{
+				visited[current] = true;
+			    Q.push(current);
+			}
+		}
+		bool operator==(const BFSIterator& rhs)
+		{
+			return current == rhs.current;
+		}
+		bool operator!=(const BFSIterator& rhs)
+		{
+			return current != rhs.current; 
+		}
+		T operator*()
+		{
+			return Q.front();
+		}
+		BFSIterator& operator++()
+		{
+			current = Q.front();
+			Q.pop();
+			for(auto neigh:adjList[current])
+			{
+				if(!visited[neigh])
+				{
+					Q.push(neigh);
+					visited[neigh] = true;
+				}
+			}
+			if(Q.empty())
+				current = T();
+			return *this;
+		}
+		
+	};
+	BFSIterator begin()
+	{
+		return BFSIterator(src,adjList);
+	}
+	BFSIterator end()
+	{
+		return BFSIterator(dummyNode,adjList);
+	}
+	friend class BFSIterator;
+};
+int main()
+{
+	Graph<int>g;
+	g.addEdge(1,2);
+	Graph<int>::BFSIterator it = find(g.begin(),g.end(),3);
+	if(it != g.end())
+		cout << "1 is found" << endl;
+	else
+		cout << "1 is not found" << endl;
+	return 0;
+}
